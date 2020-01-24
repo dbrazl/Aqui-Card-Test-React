4adc7a3c91d733f95435a76cebb2e496d33a5645
dbrazl <danielbrazlopes16@gmail.com>
2020-01-24 10:29:10 -0300

    Change endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ec2-3-81-172-109.compute-1.amazonaws.com:80',
+  baseURL: 'http://ec2-3-81-172-109.compute-1.amazonaws.com:80',
 });
 
 export default api;
